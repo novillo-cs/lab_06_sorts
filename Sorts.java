c409b4fb221362547e7d350a7705401e6327748e
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2025-12-04 23:40:07 -0500

    Update Sorts.java

--- Sorts.java
+++ Sorts.java
@@ -1,3 +1,9 @@
-public class Sorts{
+public class Sorts {
+  
+  public static void bubbleSort(int[] data) { }
 
+  public static void selectionSort(int[] data)  { }
+  
+  public static void insertionSort(int[] data)  { }
+  
 }
